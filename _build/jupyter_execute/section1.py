6167761de543b387627ea22213dc45df05c20faf
Bruce Emerson <bemerson@cocc.edu>
2021-07-01 16:22:55 -0700

    Initial repo

---
+++ _build/jupyter_execute/section1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### St Venant's Principle:
+# 
+# As we often do in introductory classes we make a number of simplifying assumptions. In the context of axial forces we are going to assume that the normal stress induced by an axial load (force) is uniformly distributed. If we look at actual distribution of the stresses in a column under axial load we see something like this...
+# 
+# <img src="https://raw.githubusercontent.com/smithrockmaker/ENGR213/master/images/Saint-Venants-Principle.jpg" width="600"/>
+# 
+# What you can see is that near the place where the force is being applied the normal stress is very non-uniform. As you move into the column/beam the stress rapidly smooths out and becomes uniform. By the time we are a distance b (the width of the column) from the point of application the stress is esssentially uniform. 
+# 
+# Saint Venant's Principle formally says that as long as we are an 'appropriate' distance from the point of application of the force then all equal external forces produce the same normal stresses in the column/beam.
+# 
+# We generally make the assumption that the normal stress is uniform to the end of the member. This is a very good approximation in almost every case.
+
+# ```{note}
+# Here is a note!
+# ```
+# ```{warning}
+# a warning
+# ```
+
+# ````{margin}
+# ```{note}
+# Here's my note!
+# ```
+# ````
